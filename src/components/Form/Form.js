bc9ee7b14edf8e6e21978b60de620e1a278bdbf2
Slim Ashkar <slim.ashkar@gmail.com>
2022-11-10 16:43:42 +0200

    Updated the Form and added 3 arrow functions

--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -6,29 +6,40 @@ import "./Form.css";
     const [formValues, setFormValues] = useState(initialValues)
 
     const handleChange = (e) => {
-        console.log(e.target);
         const { name, value } = e.target;
-        setFormValues({ ...formValues, name: value });
+        setFormValues({ ...formValues, [name]: value });
         console.log(formValues);
+    };
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+    };
+
+    const validate = (values) => {
+
     }
+
     return (
-        <form>
-            <div className="styling-form">
-                <div className="field">
-                    <label>Username</label>
-                    <input type="text" name="username" placeholder="Username" value={formValues.username} onChange={handleChange} />
+        <div className="container">
+            <pre>{JSON.stringify(formValues, undefined, 2)}</pre>
+            <form onSubmit={handleSubmit}>
+                <div className="styling-form">
+                    <div className="field">
+                        <label>Username</label>
+                        <input type="text" name="username" placeholder="Username" value={formValues.username} onChange={handleChange} />
+                    </div>
+                    <div className="field">
+                        <label>Email</label>
+                        <input type="email" name="email" placeholder="Email" value={formValues.email} onChange={handleChange} />
+                    </div>
+                    <div className="field">
+                        <label>Password</label>
+                        <input type="password" name="password" placeholder="Password" value={formValues.password} onChange={handleChange} />
+                    </div>
+                    <button className="button">Submit</button>
                 </div>
-                <div className="field">
-                    <label>Email</label>
-                    <input type="email" name="email" placeholder="Email" value={formValues.email} onChange={handleChange} />
-                </div>
-                <div className="field">
-                    <label>Password</label>
-                    <input type="password" name="password" placeholder="Password" value={formValues.password} onChange={handleChange} />
-                </div>
-                <button className="button">Submit</button>
-            </div>
-        </form>
+            </form>
+        </div>
     )
 }
 
