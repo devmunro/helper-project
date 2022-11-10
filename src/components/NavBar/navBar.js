474b3aa3e819d29e1ce9db52385574460d8185bd
David Munro <defmunro@gmail.com>
2022-11-10 14:27:57 +0000

    navBar added

--- src/components/NavBar/navBar.js
+++ src/components/NavBar/navBar.js
@@ -1,3 +1,23 @@
+import "./NavBar.css";
+
+const NavBar = () => {
+  return (
+    <div className="NavBar">
+      <h1>Helper</h1>
+      <ul className="menu">
+        <li className="menuLinks">
+          <a href="">Help Out</a>
+        </li>
+
+        <li className="menuLinks">
+          <a href="">Post Job</a>
+        </li>
+      </ul>
+    </div>
+  );
+};
+
+export default NavBar;
 
 import { Link } from "react-router-dom";
 import Search from "../SearchFilter/Search";
