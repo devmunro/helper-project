deb3493c11d28060f1808a21bd21cfff9090961e
David Munro <defmunro@gmail.com>
2022-11-11 09:46:46 +0000

    fix input text from white to black

--- src/components/Signin.js
+++ src/components/Signin.js
@@ -38,7 +38,7 @@ const Signin = () => {
           proident, sunt in culpa qui officia deserunt mollit anim id est
           laborum.
         </p>
-        
+
       </div>
       <div className="w-[50%] my-16 mr-9 p-4">
         <div>
@@ -63,7 +63,7 @@ const Signin = () => {
             <label className="py-2 font-medium">Password</label>
             <input
               onChange={(e) => setPassword(e.target.value)}
-              className="border p-3 text-black"
+              className="border p-3  text-black"
               type="password"
             />
           </div>
