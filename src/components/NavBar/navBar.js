ce1a0631ccc09a6f001ae34f8814f5d2e2126462
David Munro <defmunro@gmail.com>
2022-11-12 12:31:01 +0000

    hide searchbar on homepages, uselocation

--- src/components/NavBar/navBar.js
+++ src/components/NavBar/navBar.js
@@ -1,27 +1,32 @@
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 import Search from "../SearchFilter/Search";
 
 const NavBar = () => {
-  return (
+  const { pathname } = useLocation(); // to hide searchbar and logout
 
-    <nav className="flex justify-around text-xl p-4 bg-blue-400 text-white">
-      <Link to="/"><h1 className="font-bold p-4">Home</h1></Link>
-      {/* Added Search Component here */}
-      <Search />
-      <ul className="flex justify-around p-4">
-        <li className="mr-3 list-none">
+
+
+  
+    return (
+      <nav className="flex justify-around text-xl p-4 bg-blue-400 text-white">
+        <Link to="/">
+          <h1 className="font-bold p-4">Home</h1>
+        </Link>
+        {/* Added Search Component here */}
+        {pathname !== "/" && pathname !== "/signup" && <Search />}
+        <ul className="flex justify-around p-4">
+          <li className="mr-3 list-none">
             <Link to="/search-help">Search</Link>
-        </li>
-        <li className="mr-3 list-none">
+          </li>
+          <li className="mr-3 list-none">
             <Link to="/post-help">Post Job</Link>
-        </li>
-        <li className="mr-3 list-none">
+          </li>
+          <li className="mr-3 list-none">
             <Link to="/account">Account</Link>
-        </li>
-      </ul>
-    </nav>
-
-  );
+          </li>
+        </ul>
+      </nav>
+    );
 };
 
 export default NavBar;
