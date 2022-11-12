edac6d7ccee343876d36e19099ecd6ed66a45927
Slim Ashkar <slim.ashkar@gmail.com>
2022-11-12 02:33:32 +0200

    Search bar to navBar

--- src/components/NavBar/navBar.js
+++ src/components/NavBar/navBar.js
@@ -1,17 +1,20 @@
 
 import { Link } from "react-router-dom";
+import Search from "../SearchFilter/Search";
 
 const NavBar = () => {
   return (
-  
+
     <nav className="flex justify-around text-xl p-4 bg-blue-400 text-white">
       <Link to="/"><h1 className="font-bold p-4">Home</h1></Link>
+      {/* Added Search Component here */}
+      <Search />
       <ul className="flex justify-around p-4">
         <li className="mr-2 list-none">
-            <Link to="/search">Search</Link>
+          <Link to="/search">Search</Link>
         </li>
         <li className="mr-2 list-none">
-            <Link to="/postjob">Post Job</Link>
+          <Link to="/postjob">Post Job</Link>
         </li>
       </ul>
     </nav>
