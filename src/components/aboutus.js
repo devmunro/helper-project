1f050e9718279c318e939b80043c10f09d753373
David Munro <defmunro@gmail.com>
2022-11-13 17:49:27 +0000

    added about section

---
+++ src/components/aboutus.js
@@ -0,0 +1,28 @@
+const About = () => {
+    return ( 
+
+        <div className="flex justify-between bg-indigo-700 text-white">
+      <div className="flex flex-col w-[50%] text-center bg-hero-pattern bg-cover">
+        
+      </div>
+
+      <div className=" flex flex-col w-[50%]  my-16 mr-9 p-4 mx-auto text-center">
+        <h1 className="text-6xl py-2">About Us</h1>
+        <h2 className="text-3xl ">For the Soul</h2>
+        <p className="w-[75%] text-justify my-12 mx-auto">
+          consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
+          labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud
+          exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+          Duis aute irure dolor in reprehenderit in voluptate velit esse cillum
+          dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+          nulla pariatur. Excepteur sint occaecat cupidatat non
+          proident, sunt in culpa qui officia deserunt mollit anim id est
+          laborum.
+        </p>
+
+      </div>
+      </div>
+     );
+}
+ 
+export default About;
