7f8b6212a4039479194f8f21929e820e4628b70c
Slim Ashkar <slim.ashkar@gmail.com>
2022-11-14 12:21:31 +0200

    changed name for Search.css to Search.Style

--- src/components/SearchFilter/SearchBar.js
+++ src/components/SearchFilter/SearchBar.js
@@ -1,4 +1,6 @@
 import React from "react";
+import React, { useState, useEffect, useCallback } from "react";
+import Help from "./Help";
 
 const Search = ({
   word,
@@ -15,6 +17,33 @@ const Search = ({
     });
   };
 
+  const updateResults = useCallback(() => {
+    const arr = [];
+    if (filterData.location !== '' &&
+      filterData.category !== '' &&
+      filterData.period !== ''
+    ) {
+      const el = data.find(el => el.location === filterData.location && el.category === filterData.category && el.period === filterData.period);
+      //setDataAlreadyFiltered(el);
+      arr.push(el);
+      console.log(arr);
+      console.log(typeof dataAlreadyFiltered);
+    } else {
+      console.log('Need more specification');
+    }
+    return (
+      <Help
+        filter=""
+        data={arr}
+      />
+    )
+  }, [data, dataAlreadyFiltered, filterData.category, filterData.location, filterData.period])
+
+  useEffect(() => {
+    console.log(filterData);
+    updateResults();
+  }, [filterData, updateResults])
+
   return (
     <div className="bg-blue-300 p-4 flex lg:flex-row flex-col">
       <input
@@ -24,38 +53,36 @@ const Search = ({
         className="p-2 lg:mx-4 rounded-m m-2 lg:w-1/2 sm:w-5/6 lg:h-12"
         placeholder="Search by Job title/Location or Catergory"
       />
-      <div className="p-2 lg:mx-8 sm:mx-4 w-full ">
-        <div className="flex sm:flex-col lg:flex-row flex-row w-1/2">
-        <select
-          id="location"
-          className="p-2 rounded-m m-2"
-          value={filterData.location}
-          onChange={updateFilterData}
-          name="location"
-        >
-          <option value="">Set Location</option>
-          <option value="london">london</option>
-          <option value="paris">paris</option>
-          <option value="barcelona">barcelona</option>
-          <option value="rome">rome</option>
-        </select>
-        <select
-          id="category"
-          className="p-2 rounded-m m-2"
-          value={filterData.category}
-          onChange={updateFilterData}
-          name="category"
-        >
-          <option value="">Set Catergory</option>
-          <option value="transport">transport</option>
-          <option value="delivery">delivery</option>
-          <option value="cooking">cooking</option>
-        </select>
-        <button onClick={reset}>Reset filters</button>
-        </div>
-
-        
-      </div>
+      <select id="location" className="p-2 rounded-m m-2"
+        value={filterData.location}
+        onChange={updateFilterData}
+        name="location"
+      >
+        <option value="">--Set Location--</option>
+        <option value="london">london</option>
+        <option value="paris">paris</option>
+        <option value="barcelona">barcelona</option>
+        <option value="rome">rome</option>
+      </select>
+      <select id="category" className="p-2 rounded-m m-2"
+        value={filterData.category}
+        onChange={updateFilterData}
+        name="category"
+      >
+        <option value="">--Set Catergory--</option>
+        <option value="transport">transport</option>
+        <option value="delivery">delivery</option>
+        <option value="cooking">cooking</option>
+      </select>
+      <select id="role" className="p-2 rounded-m m-2"
+        value={filterData.period}
+        onChange={updateFilterData}
+        name="period"
+      >
+        <option value="">--Chose--</option>
+        <option value="temporal">temp</option>
+        <option value="permanent">perm</option>
+      </select>
     </div>
   );
 };
