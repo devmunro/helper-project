35456e214beba9f31c1013c7e31286a47e4eaea9
David Munro <defmunro@gmail.com>
2022-11-17 19:49:52 +0000

    applied feature now adding to applied jobs

--- src/components/Account/UserAppliedBoard.js
+++ src/components/Account/UserAppliedBoard.js
@@ -1,21 +1,25 @@
 import React, { useContext } from "react";
 import { DatabaseContext } from "../../context/DatabaseContext";
 import { UserAuth } from "../../context/AuthContext";
+import { AppliedContext } from "../../context/AppliedContext";
 import { Link } from "react-router-dom";
 
 export default function UsersApplied() {
-  const {
-        data,
-    
-  } = useContext(DatabaseContext);
+  const { data } = useContext(DatabaseContext);
+  const { appliedData } = useContext(AppliedContext);
 
   const { user } = UserAuth();
 
-  console.log(user);
-  console.log(data);
 
-  const trial = data.filter((e) => e.id === user.uid);
-  console.log(trial);
+  const matchPost = appliedData.filter((e) => e.user === user.uid);
+  
+  const result = matchPost.map((element) => {
+    return  data.filter((e) => e.id === element.PostID )
+
+  })
+
+const fin = result.map((e) => e)
+console.log(result)
 
   return (
     <div className="w-90 m-2 p-2 bg-indigo-900 ">
@@ -23,13 +27,13 @@ export default function UsersApplied() {
         Your Applied Jobs
       </h2>
       <div className="w-full flex ">
-        {trial.length > 0 &&
-          trial.map((e) => {
+        {result.length > 0 &&
+          result.map((e) => {
             return (
               <div className="w-1/3 p-4 m-2 bg-blue-100 rounded">
-                <h1 className="font-bold"> {e.title}</h1>
-                <p>{e.message}</p>
-                <p className="font-semibold">{e.location.toUpperCase()}</p>
+                <h1 className="font-bold"> {e[0].title}</h1>
+                <p>{e[0].message}</p>
+                <p className="font-semibold">{e[0].location.toUpperCase()}</p>
               </div>
             );
           })}
--- src/components/Account/apply.js
+++ src/components/Account/apply.js
@@ -3,13 +3,10 @@ import { collection, addDoc } from "firebase/firestore";
 
 import { db } from "../../firebase";
 import { UserAuth } from "../../context/AuthContext";
-import { useNavigate } from "react-router-dom";
-import { useContext } from "react";
-import {DatabaseContext} from  "../../context/DatabaseContext";
+
 
 const Apply = ({postID, postOwner}) => {
-  const navigate = useNavigate();
-  const { user } = UserAuth();
+   const { user } = UserAuth();
   const [data, setData] = useState({
     name: "",
   });
@@ -20,45 +17,32 @@ console.log(data)
   const handleSubmit = async (e) => {
     e.preventDefault();
     console.log(data);
-    const collectionRef = collection(db, "messages")
+    const collectionRef = collection(db, "applied")
     console.log(collectionRef);
     await addDoc(collectionRef, {
       ...data,
     
       user: user.uid,
       PostID : postID,
-      PostOwner: postOwner
-    });
+          });
 
  
   };
 
-  const handleChange = (e) => {
-    const { type, name, checked, value } = e.target;
-    setData((preVal) => {
-      return {
-        ...preVal,
-        [name]: type === "checkbox" ? checked : value,
-      };
-    });
-  };
+//   const handleChange = (e) => {
+//     const { type, name, checked, value } = e.target;
+//     setData((preVal) => {
+//       return {
+//         ...preVal,
+//         [name]: type === "checkbox" ? checked : value,
+//       };
+//     });
+//   };
   return (
     <div className="w-1/2">
       <form onSubmit={handleSubmit}>
-        <label htmlFor="name" className="p-1">
-          Name:
-        </label>
-        <input
-          type="text"
-          name="name"
-          id="name"
-          onChange={handleChange}
-          value={data.name}
-          placeholder="Type your name"
-         
-        />
-
-        <button type="submit">Submit</button>
+        
+        <button type="submit" className="bg-blue-200">Apply</button>
       </form>
     </div>
   );
