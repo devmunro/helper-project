66e614f06a805f070018842e5b7156f82cc867b3
David Munro <defmunro@gmail.com>
2022-11-17 11:38:18 +0000

    update colour of navbar

--- src/components/NavBar/navBar.js
+++ src/components/NavBar/navBar.js
@@ -8,7 +8,7 @@ const NavBar = () => {
   const { handleLogout } = UserAuth();
 
   return (
-    <nav className="flex justify-end text-lg p-2 bg-blue-400 text-white">
+    <nav className="flex justify-end text-lg p-2 bg-blue-600 text-white">
       <Link to="/">
         <h1 className="font-bold p-4">Home</h1>
       </Link>
