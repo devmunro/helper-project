30f17cbe803326928938377ebfd7c0af8552b65a
David Munro <defmunro@gmail.com>
2022-11-17 19:50:42 +0000

    remove add post

--- src/components/Account/UserAppliedBoard.js
+++ src/components/Account/UserAppliedBoard.js
@@ -37,13 +37,7 @@ console.log(result)
               </div>
             );
           })}
-        <Link
-          to="/account/post-help"
-          className=" flex flex-col w-max p-4 m-2 bg-blue-100 border-black border-dotted border-2 text-center hover:scale-105 hover:border-l-8 hover:border-r-8"
-        >
-          <p className="font-extrabold text-7xl">+</p>
-          <p>Add Post</p>
-        </Link>
+       
       </div>
     </div>
   );
