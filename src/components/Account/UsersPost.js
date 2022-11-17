dd71bde00d95c563e83ce5e7d320d8e3f3811568
David Munro <defmunro@gmail.com>
2022-11-17 10:00:44 +0000

    add userspost file

---
+++ src/components/Account/UsersPost.js
@@ -0,0 +1,35 @@
+import React, { useContext } from 'react'
+import { DatabaseContext } from '../../context/DatabaseContext';
+import { UserAuth } from '../../context/AuthContext';
+
+export default function UsersPost() {
+
+    
+   
+        const {
+          updateSearch,
+          searchTerm,
+          filterData,
+          updateFilterData,
+          data,
+          getUsers,
+          setFilterData,
+          searchValue,
+          dataAlreadyFiltered,
+        } = useContext(DatabaseContext)
+
+        const { user } = UserAuth();
+      
+
+        console.log(user)
+        console.log(data)
+
+        const trial = data.filter((e) => e.user === user.uid)
+ console.log(trial)
+
+  return (
+    trial.map((e) => {return (
+        <div>{e.name}</div>
+    )})
+  )
+}
