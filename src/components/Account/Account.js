2cf3231ce0b3c9ff11b0016c2d6d20cec5e2cccf
David Munro <defmunro@gmail.com>
2022-11-17 10:00:23 +0000

    create function for userposts

--- src/components/Account/Account.js
+++ src/components/Account/Account.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { UserAuth } from "../../context/AuthContext";
 import { Link } from "react-router-dom";
+import UsersPost from "./UsersPost";
 
 const Account = () => {
   const { user } = UserAuth();
@@ -54,6 +55,7 @@ const Account = () => {
           </div>
         </Link>
       </div>
+      <UsersPost />
     </div>
   );
 };
