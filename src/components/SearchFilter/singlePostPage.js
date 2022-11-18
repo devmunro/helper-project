cdcb7642861ff8474bf1b68b1d9a38fc44257e84
David Munro <defmunro@gmail.com>
2022-11-18 11:08:40 +0000

    create form for applying for role

--- src/components/SearchFilter/singlePostPage.js
+++ src/components/SearchFilter/singlePostPage.js
@@ -13,33 +13,48 @@ export default function SinglePostPage() {
   const [singlePost, setSinglePost] = useState([]);
   const [loading, setLoading] = useState(true);
 
-  
-
   useEffect(() => {
     const single = () => {
+      const matchPage = data.filter((e) => e.id === id);
+      console.log(matchPage);
+      setSinglePost(matchPage);
 
-        const matchPage = data.filter((e) => e.id === id);
-        console.log(matchPage)
-    setSinglePost(matchPage)
-
-   
-        setLoading(false)
-      }
-      ;
-
-    single()
+      setLoading(false);
+    };
+    single();
   }, [data, id]);
 
- 
+  console.log(data);
 
   console.log(singlePost.title);
   return (
     <div>
-      <Link className="font-bold "to="/account/search-help">BACK</Link>
+      <Link className="font-bold " to="/account/search-help">
+        BACK
+      </Link>
       {loading && <p>...loading</p>}
-      {!loading && singlePost.map((e) => {return (<div><div>{e.title} <Apply postID={e.id}/></div> 
-      </div>)})}
-      
+      {!loading &&
+        singlePost.map((e) => {
+          return (
+            <div className="flex w-full m-4 p-2 justify-evenly">
+              <div className="w-1/2 text-justify 	">
+                <h2 className="font-bold p-2">{e.title} </h2>
+                <p>{e.message}</p>
+                <ul className="flex font-bold space-x-2 m-2 p-2 justify-center">
+                  <li>{e.location.toUpperCase()}</li>
+                  <li>{e.name.toUpperCase()} & {e.age}</li>
+                  <li>applied</li>
+                </ul>
+              </div>
+              <div className="w-1/2">
+                {" "}
+                <button>
+                  <Apply postID={e.id} />{" "}
+                </button>{" "}
+              </div>
+            </div>
+          );
+        })}
     </div>
   );
 }
