f9863d6dbedb132d2a06d29402657fe50a56856c
David Munro <defmunro@gmail.com>
2022-11-18 12:09:04 +0000

    remove links

--- src/components/Account/Account.js
+++ src/components/Account/Account.js
@@ -27,38 +27,7 @@ const Account = () => {
       
       <UsersPost />
       <UsersApplied/>
-      <div className="flex w-1/2 mt-10">
-        <Link to="/account/post-help">
-          <div className="w-1/2 rounded shadow-2xl hover:scale-105">
-            <img
-              className="w-full"
-              src="https://images.unsplash.com/photo-1618218168350-6e7c81151b64?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1374&q=80"
-              alt="ASK FOR HELP, scrabble pieces"
-            />
-            <div className="px-6 py-4">
-              <div className="font-bold text-2xl mb-2">
-                Need a helping hand?{" "}
-                <span className="text-sm px-4">ClickHere</span>
-              </div>
-            </div>
-          </div>
-        </Link>
-        <Link to="/account/search-help">
-          <div class="w-1/2 rounded shadow-2xl hover:scale-105">
-            <img
-              class="w-full"
-              src="https://images.unsplash.com/photo-1611223235982-891064f27716?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1374&q=80"
-              alt="scrabble pieces"
-            />
-            <div class="px-6 py-4">
-              <div class="font-bold text-2xl mb-2">
-                Can you offer assistance?{" "}
-                <span className="text-sm px-4">ClickHere</span>
-              </div>
-            </div>
-          </div>
-        </Link>
-      </div>
+      
       
     </div>
   );
