4dc878d793c28b52114add98355aea8ecf8d6247
David Munro <defmunro@gmail.com>
2022-11-19 12:56:54 +0000

    improve searchbar design

--- src/components/SearchFilter/SearchBar.js
+++ src/components/SearchFilter/SearchBar.js
@@ -1,53 +1,60 @@
-import React,{} from "react";
+import React from "react";
 
-
-const Search = ({ word, update, filterData, updateFilterData, getUsers, setFilterData}) => {
-
-
-
-  const reset=()=>{
-   
+const Search = ({
+  word,
+  update,
+  filterData,
+  updateFilterData,
+  getUsers,
+  setFilterData,
+}) => {
+  const reset = () => {
     setFilterData({
-       location:'',
-       category:''
-     })
-  }
+      location: "",
+      category: "",
+    });
+  };
 
   return (
-    <div className="bg-blue-300 p-4">
+    <div className="bg-blue-300 p-4 flex lg:flex-row flex-col">
       <input
         type="text"
         ref={word}
         onChange={update}
-        className="p-2 mx-10 rounded-m m-2 w-1/2"
+        className="p-2 mx-10 rounded-m m-2 lg:w-1/2 w-2/3"
         placeholder="Search by Job title/Location or Catergory"
       />
-      <select  id="location" className="p-2 rounded-m m-2"
-         value={filterData.location}
-         onChange={updateFilterData} 
-         name="location" 
-      >
-        <option value="">--Set Location--</option>
-        <option value="london">london</option>
-        <option value="paris">paris</option>
-        <option value="barcelona">barcelona</option>
-        <option value="rome">rome</option>
-      </select>
-      <select id="category" className="p-2 rounded-m m-2"
-         value={filterData.category}
-         onChange={updateFilterData}
-         name="category" 
-      >
-        <option value="">--Set Catergory--</option>
-        <option value="transport">transport</option>
-        <option value="delivery">delivery</option>
-        <option value="cooking">cooking</option>
-      </select>
+      <div className="p-2 mx-10">
+        <select
+          id="location"
+          className="p-2 rounded-m m-2"
+          value={filterData.location}
+          onChange={updateFilterData}
+          name="location"
+        >
+          <option value="">--Set Location--</option>
+          <option value="london">london</option>
+          <option value="paris">paris</option>
+          <option value="barcelona">barcelona</option>
+          <option value="rome">rome</option>
+        </select>
+        <select
+          id="category"
+          className="p-2 rounded-m m-2"
+          value={filterData.category}
+          onChange={updateFilterData}
+          name="category"
+        >
+          <option value="">--Set Catergory--</option>
+          <option value="transport">transport</option>
+          <option value="delivery">delivery</option>
+          <option value="cooking">cooking</option>
+        </select>
 
-      <button onClick={reset}>Reset filters</button>
-          </div>
+        <button onClick={reset}>Reset filters</button>
+      </div>
+    </div>
   );
 };
 
 export default Search;
-
