86a15f4fd83ba79690110209de162c8f737d20a4
David Munro <defmunro@gmail.com>
2022-11-22 15:05:56 +0000

    add new icons to single post page

--- src/components/SearchFilter/singlePostPage.js
+++ src/components/SearchFilter/singlePostPage.js
@@ -7,6 +7,8 @@ import Apply from "../Account/apply";
 import { useNavigate } from "react-router-dom";
 import { deleteDoc, doc, getDocs, collection } from "firebase/firestore";
 import { db } from "../../firebase";
+import skyscraper from "../../icons/SkyscraperIcon.png";
+import info from "../../icons/Infoicon.png";
 
 export default function SinglePostPage() {
   const { data } = useContext(DatabaseContext);
@@ -103,13 +105,15 @@ export default function SinglePostPage() {
                   d="M12 9v3.75m-9.303 3.376c-.866 1.5.217 3.374 1.948 3.374h14.71c1.73 0 2.813-1.874 1.948-3.374L13.949 3.378c-.866-1.5-3.032-1.5-3.898 0L2.697 16.126zM12 15.75h.007v.008H12v-.008z"
                 />
               </svg>
-              <h2 className=" font-bold text-xl my-4">ARE YOU SURE YOU WANT TO DELETE!!!</h2>
+              <h2 className=" font-bold text-xl my-4">
+                ARE YOU SURE YOU WANT TO DELETE!!!
+              </h2>
             </div>
             <div className="flex justify-center ">
               <button
                 value="yes"
                 onClick={handleBox}
-                className= "rounded bg-blue-800 font-bold text-white px-4 p-2 m-2 block shadow-lg"
+                className="rounded bg-blue-800 font-bold text-white px-4 p-2 m-2 block shadow-lg"
               >
                 YES
               </button>
@@ -136,56 +140,34 @@ export default function SinglePostPage() {
               <div className=" w-full lg:w-1/2 lg:mx-4 text-justify border-2 p-4 shadow-lg	">
                 <h2 className="font-bold p-2 bg-blue-400">{e.title} </h2>
                 <ul className="flex-col font-bold  text-white m-2 p-2 justify-center text-sm ">
-                  <li className="flex space-x-3 p-2 m-1 bg-blue-400 w-max rounded-full ">
+                  <li className="flex space-x-3 p-2 m-1 bg-blue-400 w-max rounded-full items-center	">
                     <div>
-                    <svg
-                      xmlns="http://www.w3.org/2000/svg"
-                      fill="none"
-                      l
-                      stroke="currentColor"
-                      class="w-6 h-6"
-                    >
-                      <path
-                        strokeLinecap="round"
-                        strokeLinejoin="round"
-                        d="M2.25 21h19.5m-18-18v18m10.5-18v18m6-13.5V21M6.75 6.75h.75m-.75 3h.75m-.75 3h.75m3-6h.75m-.75 3h.75m-.75 3h.75M6.75 21v-3.375c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21M3 3h12m-.75 4.5H21m-3.75 3.75h.008v.008h-.008v-.008zm0 3h.008v.008h-.008v-.008zm0 3h.008v.008h-.008v-.008z"
-                      />
-                    </svg>
+                      <img src={skyscraper} alt="icon of skyscraper"></img>
                     </div>
                     Location: {e.location.toUpperCase()}
                   </li>
-                  <li className="flex space-x-3 p-2 m-1 bg-blue-400 w-max rounded-full">
-                    <svg
-                      xmlns="http://www.w3.org/2000/svg"
-                      fill="none"
-                      viewBox="0 0 24 24"
-                      strokeWidth="1.5"
-                      stroke="currentColor"
-                      class="w-6 h-6"
-                    >
-                      <path
-                        strokeLinecap="round"
-                        strokeLinejoin="round"
-                        d="M15.666 3.888A2.25 2.25 0 0013.5 2.25h-3c-1.03 0-1.9.693-2.166 1.638m7.332 0c.055.194.084.4.084.612v0a.75.75 0 01-.75.75H9a.75.75 0 01-.75-.75v0c0-.212.03-.418.084-.612m7.332 0c.646.049 1.288.11 1.927.184 1.1.128 1.907 1.077 1.907 2.185V19.5a2.25 2.25 0 01-2.25 2.25H6.75A2.25 2.25 0 014.5 19.5V6.257c0-1.108.806-2.057 1.907-2.185a48.208 48.208 0 011.927-.184"
-                      />
-                    </svg>
+                  <li className="flex space-x-3 p-2 m-1 bg-blue-400 w-max rounded-full items-center	">
+                    <img src={info} alt="icon of info sign"></img>
                     Short Bio: {e.name.toUpperCase()} & {e.age}
                   </li>
-                  
                 </ul>
-                <div className="flex justify-between"> 
-                <h3 className="font-bold md:px-10 md:mx-8">Description</h3>
-                <span>{postee.length > 0 && (
-                  <button className="font-bold italic text-black mx-5" onClick={handleBox}>
-                    DELETE
-                  </button>
-                )}
-                {/*REMOVES DELETE BUTTON FOR POTENTIAL APPLIER*/}</span>
+                <div className="flex justify-between">
+                  <h3 className="font-bold md:px-10 md:mx-8">Description</h3>
+                  <span>
+                    {postee.length > 0 && (
+                      <button
+                        className="font-bold italic text-black mx-5"
+                        onClick={handleBox}
+                      >
+                        DELETE
+                      </button>
+                    )}
+                    {/*REMOVES DELETE BUTTON FOR POTENTIAL APPLIER*/}
+                  </span>
                 </div>
                 <p className="p-4 m-1 bg-blue-300 shadow-sm rounded-lg md:px-10 md:mx-8">
                   {e.message}
                 </p>
-                
               </div>
 
               {/* // Checking if user has posted this, if so Appliction form does not show */}
